37830f1896609f476bca1b939fae52de4c0cbf88
franciscoarocas <54645175+franciscoarocas@users.noreply.github.com>
2023-02-12 23:34:25 +0100

    Added second cover

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -31,6 +31,17 @@ const Home = () => {
           }
         </div>
       </div>
+      <div className="secondCover spaceBetween">
+        <div>
+          <h1>Little Lemon Restaurant</h1>
+          <h2>Chicago</h2>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        </div>
+        <div style={{position : "relative"}}>
+          <img src="/icons_assets/restaurant.jpg" className="imgA" />
+          <img src="/icons_assets/restaurant chef B.jpg" className="imgB" />
+        </div>
+      </div>
     </Template>
   );
 
