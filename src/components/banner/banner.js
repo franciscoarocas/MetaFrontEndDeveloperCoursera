65eaa789f1f696cd0f568df6a7c892f29e91c40b
franciscoarocas <54645175+franciscoarocas@users.noreply.github.com>
2023-02-12 23:49:11 +0100

    Finished footer

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -1,3 +1,4 @@
+import Button from "../button/button";
 
 const Banner = () => {
 
@@ -6,7 +7,10 @@ const Banner = () => {
       <div>
         <h1>Little Lemon Restaurant</h1>
         <h2>Chicago</h2>
-        <p></p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <Button>
+          Reserve a table
+        </Button>
       </div>
       <img src="/icons_assets/Mario and Adrian A.jpg"/>
     </div>
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,7 +4,29 @@ const Footer = () => {
   return (
     <footer className="spaceBetween">
       <span><img src="/icons_assets/Logo.svg" alt="Little Lemon Logo"/></span>
-      <span>Little Lemon - 2023</span>
+      <ul>
+        <h3>Doormat Navigation</h3>
+        <li>Home</li>
+        <li>About</li>
+        <li>Menu</li>
+        <li>Reservations</li>
+        <li>Order Online</li>
+        <li>Login</li>
+      </ul>
+      <ul>
+        <h3>Contact</h3>
+        <li>Address</li>
+        <li>Phone number</li>
+        <li>Email</li>
+      </ul>
+      <ul>
+        <h3>Social Media Links</h3>
+        <li>Facebook</li>
+        <li>Twitter</li>
+        <li>Phone Number</li>
+        <li>WhatsApp</li>
+        <li>Telegram</li>
+      </ul>
     </footer>
   )
 
