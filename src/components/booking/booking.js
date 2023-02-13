bb584948bfb684d8052db0789b9b2cec231cd24f
franciscoarocas <54645175+franciscoarocas@users.noreply.github.com>
2023-02-13 23:05:48 +0100

    Added react booking and booking page

---
+++ src/components/booking/booking.js
@@ -0,0 +1,14 @@
+
+import Template from "../template/template";
+
+const Booking = () => {
+
+  return (
+    <Template>
+      BOOKING
+    </Template>
+  );
+
+};
+
+export default Booking;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import './App.css';
 import reportWebVitals from './reportWebVitals';
 
+import Home from './components/home/home';
+import Booking from './components/booking/booking';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/booking",
+    element: <Booking/>,
+  }
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
